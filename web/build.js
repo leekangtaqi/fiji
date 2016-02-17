40153a0a84d6fc3fb6c927534d5d045d28975eb7
leekangtaqi <leekangtaqi@sina.com>
2016-02-17 17:19:56 +0800

    init

--- web/build.js
+++ web/build.js
@@ -2,6 +2,15 @@ import * as React from 'react';
 import * as ReactDom from 'react-dom';
 
 class HelloWorld extends React.Component {
+    constructor(props) {
+        super();
+        this.status = {};
+        this.status.test = props.test;
+        console.log(typeof this.status.test);
+    }
+    xxx() {
+        alert('1111');
+    }
     render() {
         return React.createElement(
             'div',
@@ -9,9 +18,15 @@ class HelloWorld extends React.Component {
             React.createElement(
                 'span',
                 null,
-                '111'
+                'hello world'
+            ),
+            React.createElement('br', null),
+            React.createElement(
+                'span',
+                { onClick: this.xxx },
+                this.status.test.name
             )
         );
     }
 }
-ReactDom.render(React.createElement(HelloWorld, null), document.getElementById('app'));
+ReactDom.render(React.createElement(HelloWorld, { test: { name: '111' } }), document.getElementById('app'));
--- web/src/boot.jsx
+++ web/src/boot.jsx
@@ -2,15 +2,25 @@ import * as React from 'react';
 import * as ReactDom from 'react-dom';
 
 class HelloWorld extends React.Component{
+    constructor(props){
+        super();
+        this.status = {};
+        this.status.test = props.test; 
+        console.log(typeof this.status.test)
+    }
+    xxx(){
+        alert('1111');
+    }
     render(){
         return(
             <div>
-                <span>111</span>
+                <span>hello world</span><br/>
+                <span onClick={this.xxx}>{this.status.test.name}</span>
             </div>  
         )  
     }
 }  
 ReactDom.render(
-    <HelloWorld/>,
+    <HelloWorld test={{name: '111'}}/>,
     document.getElementById('app')
 )
